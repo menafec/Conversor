2a3ac0d0b89aeae829a8acccb0b11e09895e0e01
Ximena Fernandez <124226344+menafec@users.noreply.github.com>
2023-03-02 12:54:04 -0600

    Add files via upload

---
+++ Conversor.py
@@ -0,0 +1,17 @@
+#app para convertir pies a metros usando Tkinter
+#Ximena Fernandez
+#23 Feb 2023
+
+from tkinter import *
+from tkinter import ttk
+
+class Conv: 
+
+    #tipo de constructor de la clase
+    def __init__(self, raiz):
+        raiz.title("Pies a Metros")
+
+if __name__=="__main__":
+    print("Este es el archivo principal.")
+    print("Nada más se mostrará esto si es el principal.")
+
---
+++ Principal.py
@@ -0,0 +1,9 @@
+#construir ventana iniciar constructor e iniciarla
+
+from tkinter import *
+from tkinter import Tk
+from Conversor import Conv
+
+raiz = Tk()
+Conv(raiz)
+raiz.mainloop()
